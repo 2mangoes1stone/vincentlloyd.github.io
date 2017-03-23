c1d4efe29ec84bc1ad8848c8c7dcaaf4aa3d8416
Vincent Lloyd <vincent.lloyd@protonmail.com>
2017-03-23 14:25:35 +1100

    Edit: index.html, styles.css, myscripts.js, all svgs ('cleaned' using svgo)

--- myscripts.js
+++ myscripts.js
@@ -1,3 +1,14 @@
 AOS.init({
   duration: 1200,
-})
+});
+
+// Toolbar show / hide
+document.addEventListener("scroll", function() {
+  if (window.pageYOffset > 1000)
+    document.getElementById('toolbar').style.display = "block";
+});
+
+document.addEventListener("scroll", function() {
+  if (window.pageYOffset < 1000)
+    document.getElementById('toolbar').style.display = "none";
+});
