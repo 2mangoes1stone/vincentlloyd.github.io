b732a5df85fb1b6a60aa8fc6a423d004fe8b5902
Vincent Lloyd <vincent.lloyd@protonmail.com>
2017-03-24 19:51:07 +1100

    Edit index.html, styles.css, myscripts.js, moire-03.svg; Create: kevin.png, tappy-cat.gif, tony.png

--- myscripts.js
+++ myscripts.js
@@ -27,27 +27,30 @@
 var palettes = [ ["#e0e0e0", "#e8122f"], ["#000000", "#f9cd43"], ["#e8122f", "#000000"], ["#2e00fd", "#ff9600"], ["#e8122f", "#e0e0e0"], ["#003967", "#f2b300"], ["#00613f", "#ff6141"], ["#f9cd43", "#e8122f"] ];
 
 // Random colour palette selector
-function GetValue() {
+function setColour() {
   var newPalette = palettes[Math.floor(Math.random() * palettes.length)];
   //alert(random);
   document.getElementById("message").innerHTML=newPalette;
+
 };
 
 // Toolbar show
 document.addEventListener("scroll", function() {
-  if (window.pageYOffset > 1000)
+  if (window.pageYOffset > 200)
     document.getElementById('toolbar').style.display = "block";
 });
 
 // Toolbar hide
 document.addEventListener("scroll", function() {
-  if (window.pageYOffset < 1000)
+  if (window.pageYOffset < 200)
     document.getElementById('toolbar').style.display = "none";
 });
 
 AOS.init({
   duration: 1200,
 });
+
+
 
 
 // , ["#14f7a4", "#292929"], ["#292929", "#f7484e"]
