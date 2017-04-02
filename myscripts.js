be631c10550da9fb71d52b89c35a1a8c1a6cb0de
Vincent Lloyd <vincent.lloyd@protonmail.com>
2017-04-02 11:10:13 +1000

    EDIT: index.html, style.css, myscripts.js, most images renamed

--- myscripts.js
+++ myscripts.js
@@ -23,20 +23,28 @@
 //   }, 10);
 // };
 
+
+// COLOUR THEME SELECTOR
+
+
 // Altenate colour palettes
 var palettes = [ ["#e0e0e0", "#e8122f"], ["#000000", "#f9cd43"], ["#e8122f", "#000000"], ["#2e00fd", "#ff9600"], ["#e8122f", "#e0e0e0"], ["#003967", "#f2b300"], ["#00613f", "#ff6141"], ["#f9cd43", "#e8122f"] ];
+// , ["#14f7a4", "#292929"], ["#292929", "#f7484e"]
 
 // Random colour palette selector
 function setColour() {
   var newPalette = palettes[Math.floor(Math.random() * palettes.length)];
   //alert(random);
   document.getElementById("message").innerHTML=newPalette;
-
 };
+
+
+// TOOLBAR SHOW / HIDE
+
 
 // Toolbar show
 document.addEventListener("scroll", function() {
-  if (window.pageYOffset > 41)
+  if (window.pageYOffset >= 41)
     document.getElementById('toolbar').style.display = "block";
 });
 
@@ -46,11 +54,30 @@ document.addEventListener("scroll", function() {
     document.getElementById('toolbar').style.display = "none";
 });
 
-AOS.init({
-  duration: 1200,
-});
+
+// BOOTCAMP GRADUATION COUNDOWN TIMER
 
 
+// Bootcamp graduation date
+var targetDate = new Date("Aug 25, 2017 17:00:00").getTime();
 
+// Refresh every second
+var x = setInterval(function() {
 
-// , ["#14f7a4", "#292929"], ["#292929", "#f7484e"]
+  // Today's date and time remaining variables
+  var now = new Date().getTime();
+  var timeLeft = targetDate - now;
+
+  // Days, hours, minutes, seconds remaining in countdown
+  var day = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+  var hr = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var min = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+  var sec = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+  // Set values of appropiate html elements
+  document.getElementById("day").innerHTML = day;
+  document.getElementById("hr").innerHTML = hr;
+  document.getElementById("min").innerHTML = min;
+  document.getElementById("sec").innerHTML = sec;
+
+}, 1000);
